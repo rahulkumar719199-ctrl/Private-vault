66387739fb2dc76f90b631010177dd81d417281c
rahul <rahulkumar719199@gmail.com>
2026-08-24 11:08:59 +0530

    Fix: Use dynamic port for Render

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const crypto = require('crypto');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const DB_PATH = './db.json';
 const PHOTO_DIR = './photos';
 
